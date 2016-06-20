ce13b9f1ae3f725b8f06510f492d10fbd4a1f6e0
Andrew Turner <aturner@cs.utexas.edu>
2016-06-20 17:24:51 -0500

    Pushing to find out if build works on travis with new requests lib

--- Netflix.py
+++ Netflix.py
@@ -1,46 +1,63 @@
 #!/usr/bin/env python3
-
+import sys
+import os
 import pickle
+from urllib.request import urlopen
 from numpy import mean, sqrt, square, subtract
 
-#The cache with the answers to check for {int:{int:int}}
-ANSWERS_CACHE = pickle.load(open("/u/downing/cs/netflix-caches/amm6364-answer.p", "rb"))
-#The cache with the average customer ratings {int:float}
-CUSTOMER_RATINGS = pickle.load(open("/u/downing/cs/netflix-caches/amm6364-averageCustomerRating.p", "rb"))
-#The cache with the average movie ratings {int:float}
-MOVIE_RATINGS = pickle.load(open("/u/downing/cs/netflix-caches/amm6364-averageMovieRating.p", "rb"))
+filepaths = [
+    "/u/downing/cs/netflix-caches/amm6364-answer.p",
+    "/u/downing/cs/netflix-caches/amm6364-averageCustomerRating.p",
+    "/u/downing/cs/netflix-caches/amm6364-averageMovieRating.p"
+    ]
+
+urlpaths = [
+    "http://www.cs.utexas.edu/users/downing/netflix-caches/amm6364-answer.p",
+    "http://www.cs.utexas.edu/users/downing/netflix-caches/amm6364-averageCustomerRating.p",
+    "http://www.cs.utexas.edu/users/downing/netflix-caches/amm6364-averageMovieRating.p"
+    ]
+
+
+if os.path.isfile(filepaths[0]):
+    ANSWERS_CACHE = pickle.load(open(filepaths[0], "rb"))
+    CUSTOMER_RATINGS = pickle.load(open(filepaths[1], "rb"))
+    MOVIE_RATINGS = pickle.load(open(filepaths[2], "rb"))
+else:
+    ANSWERS_CACHE = pickle.load(urlopen(urlpaths[0]))
+    CUSTOMER_RATINGS = pickle.load(urlopen(urlpaths[1]))
+    MOVIE_RATINGS = pickle.load(urlopen(urlpaths[2]))
 
 ANSWERS_LIST = []
 RATINGS_LIST = []
 
 
 def netflix_predict(movie_id, cust_id, writer):
-	global ANSWERS_LIST
-	global RATINGS_LIST
+    global ANSWERS_LIST
+    global RATINGS_LIST
 
-	customer_avg = CUSTOMER_RATINGS.get(cust_id)
-	movie_avg = MOVIE_RATINGS.get(movie_id)
-	total_avg = round((customer_avg + movie_avg) / 2, 1)
+    customer_avg = CUSTOMER_RATINGS.get(cust_id)
+    movie_avg = MOVIE_RATINGS.get(movie_id)
+    total_avg = round((customer_avg + movie_avg) / 2, 1)
 
-	RATINGS_LIST.append(total_avg)
-	ANSWERS_LIST.append(ANSWERS_CACHE.get(movie_id).get(cust_id))
-	writer.write(str(total_avg) + "\n")
+    RATINGS_LIST.append(total_avg)
+    ANSWERS_LIST.append(ANSWERS_CACHE.get(movie_id).get(cust_id))
+    writer.write(str(total_avg) + "\n")
 
 def netflix_solve(reader, writer):
-	"""
-	reader, reader to get input from
-	writer, writer to write to output
-	"""
-	movie_id = 0
-	for line in reader:
-		if ":" in line:
-			movie_id = int(''.join(c for c in line if c.isdigit()))
-			writer.write("".join(line.split()) + "\n")
-		else:
-			netflix_predict(movie_id, int(line), writer)
+    """
+    reader, reader to get input from
+    writer, writer to write to output
+    """
+    movie_id = 0
+    for line in reader:
+        if ":" in line:
+            movie_id = int(''.join(c for c in line if c.isdigit()))
+            writer.write("".join(line.split()) + "\n")
+        else:
+            netflix_predict(movie_id, int(line), writer)
 
-	rsme = netflix_rsme()
-	writer.write("RMSE: " + str(round(rsme, 2)) + "\n")
+    rsme = netflix_rsme()
+    writer.write("RMSE: " + str(round(rsme, 2)) + "\n")
 
 def netflix_rsme():
-	return sqrt(mean(square(subtract(ANSWERS_LIST, RATINGS_LIST))))
+    return sqrt(mean(square(subtract(ANSWERS_LIST, RATINGS_LIST))))
